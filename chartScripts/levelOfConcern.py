ce3f4f6cc1d06bed06768a460a177a1468687c07
Anna Warno <anna.warno@7bulls.com>
2020-06-01 13:06:30 +0200

    added smoothed line

--- chartScripts/levelOfConcern.py
+++ chartScripts/levelOfConcern.py
@@ -2,6 +2,8 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
 import pandas as pd
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d
 
 
 def levelOfConcern(mode='total_cases'):
@@ -18,17 +20,23 @@ def levelOfConcern(mode='total_cases'):
                              mode='lines',
                              name='Level of concern'), secondary_y=True)
 
+
+    fig.add_trace(go.Scatter(x=df_con["date"], y=gaussian_filter1d(np.array(df_con["United Kingdom"].values), sigma=3),
+                             mode='lines',
+                             name='Smoothed level of concern', marker_color='#2ca02c'), secondary_y=True)
+
+
     fig.add_trace(
         go.Bar(name='United Kingdom' +  mode.split('_')[0] + ' ' + mode.split('_')[1], x=df1uk[df1uk.date >= '2020-03-24']["date"], y=df1uk[mode],
-               opacity=0.60),)
+               opacity=0.90, marker_color='#ce4c36'))
 
 
 
 
     fig.update_xaxes(title_text="Date")
     fig.update_yaxes(title_text="Daily new cases in UK", secondary_y=False)
-    fig.update_yaxes(title_text="Level of Concern (from 0 to 10)", secondary_y=True)
-
+    fig.update_yaxes(title_text="Level of Concern (from 0 to 10)", range=[5, 10], secondary_y=True)
+ 
     fig.update_layout(
         height=400,
         showlegend=True,
